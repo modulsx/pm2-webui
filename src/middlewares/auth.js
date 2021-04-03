cde6780da857d542384f4c8a7aa3afc6feec9610
Surya T <suryamodulus@gmail.com>
2021-04-03 13:43:25 +0000

    fix login authenticated redirect

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,6 +2,9 @@ const isAuthenticated = async (ctx, next) => {
     if(ctx.url !== "/login" && !ctx.session.isAuthenticated){
         return ctx.redirect('/login')
     }
+    else if(ctx.url === "/login" && ctx.session.isAuthenticated){
+        return ctx.redirect('/apps')
+    }
     await next()
 }
 
