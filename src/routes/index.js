7f52f1d5fa01c322aa96bdc43af0c2fe6c1b5671
deepaksveltetech <deepak@sveltetech.com>
2022-09-16 15:49:50 +0530

    add realtime logs.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,157 +1,175 @@
-const config = require('../config')
-const RateLimit = require('koa2-ratelimit').RateLimit;
-const router = require('@koa/router')();
-const { listApps, describeApp, reloadApp, restartApp, stopApp } = require('../providers/pm2/api')
-const { validateAdminUser } = require('../services/admin.service')
-const  { readLogsReverse } = require('../utils/read-logs.util')
-const { getCurrentGitBranch, getCurrentGitCommit } = require('../utils/git.util')
-const { getEnvFileContent } = require('../utils/env.util')
-const { isAuthenticated, checkAuthentication }= require('../middlewares/auth')
-const AnsiConverter = require('ansi-to-html');
+const config = require("../config");
+const RateLimit = require("koa2-ratelimit").RateLimit;
+const router = require("@koa/router")();
+const {
+  listApps,
+  describeApp,
+  reloadApp,
+  restartApp,
+  stopApp,
+} = require("../providers/pm2/api");
+const { validateAdminUser } = require("../services/admin.service");
+const { readLogsReverse } = require("../utils/read-logs.util");
+const {
+  getCurrentGitBranch,
+  getCurrentGitCommit,
+} = require("../utils/git.util");
+const { getEnvFileContent } = require("../utils/env.util");
+const { isAuthenticated, checkAuthentication } = require("../middlewares/auth");
+const AnsiConverter = require("ansi-to-html");
 const ansiConvert = new AnsiConverter();
 
 const loginRateLimiter = RateLimit.middleware({
-    interval: 2*60*1000, // 2 minutes
-    max: 100,
-    prefixKey: '/login' // to allow the bdd to Differentiate the endpoint 
+  interval: 2 * 60 * 1000, // 2 minutes
+  max: 100,
+  prefixKey: "/login", // to allow the bdd to Differentiate the endpoint
+});
+
+router.get("/", async (ctx) => {
+  return ctx.redirect("/login");
+});
+
+router.get("/login", loginRateLimiter, checkAuthentication, async (ctx) => {
+  return await ctx.render("auth/login", {
+    layout: false,
+    login: { username: "", password: "", error: null },
   });
+});
 
-router.get('/', async (ctx) => {
-    return ctx.redirect('/login')
-})
-
-router.get('/login', loginRateLimiter, checkAuthentication, async (ctx) => {
-    return await ctx.render('auth/login', {layout : false, login: { username: '', password:'', error: null }})
-})
-
-router.post('/login', loginRateLimiter, checkAuthentication, async (ctx) => {
-    const { username, password } = ctx.request.body;
-    try {
-        await validateAdminUser(username, password)
-        ctx.session.isAuthenticated = true;
-        return ctx.redirect('/apps')
-    }
-    catch(err){
-        return await ctx.render('auth/login', {layout : false, login: { username, password, error: err.message }})
-    }
-})
-
-router.get('/apps', isAuthenticated, async (ctx) => {
-    const apps =  await listApps()
-    return await ctx.render('apps/dashboard', {
-      apps
+router.post("/login", loginRateLimiter, checkAuthentication, async (ctx) => {
+  const { username, password } = ctx.request.body;
+  try {
+    await validateAdminUser(username, password);
+    ctx.session.isAuthenticated = true;
+    return ctx.redirect("/apps");
+  } catch (err) {
+    return await ctx.render("auth/login", {
+      layout: false,
+      login: { username, password, error: err.message },
     });
+  }
 });
 
-router.get('/logout', (ctx)=>{
-    ctx.session = null;
-    return ctx.redirect('/login')
-})
-
-router.get('/apps/:appName', isAuthenticated, async (ctx) => {
-    const { appName } = ctx.params
-    let app =  await describeApp(appName)
-    if(app){
-        app.git_branch = await getCurrentGitBranch(app.pm2_env_cwd)
-        app.git_commit = await getCurrentGitCommit(app.pm2_env_cwd)
-        app.env_file = await getEnvFileContent(app.pm2_env_cwd)
-        const stdout = await readLogsReverse({filePath: app.pm_out_log_path})
-        const stderr = await readLogsReverse({filePath: app.pm_err_log_path})
-        stdout.lines = stdout.lines.map(log => {
-            return  ansiConvert.toHtml(log)
-        }).join('<br/>')
-        stderr.lines = stderr.lines.map(log => {
-            return  ansiConvert.toHtml(log)
-        }).join('<br/>')
-        return await ctx.render('apps/app', {
-            app,
-            logs: {
-                stdout,
-                stderr
-            }
-        });
-    }
-    return ctx.redirect('/apps')
+router.get("/apps", isAuthenticated, async (ctx) => {
+  const apps = await listApps();
+  return await ctx.render("apps/dashboard", {
+    apps,
+  });
 });
 
-router.get('/api/apps/:appName/logs/:logType', isAuthenticated, async (ctx) => {
-    const { appName, logType } = ctx.params
-    const { linePerRequest, nextKey } = ctx.query
-    if(logType !== 'stdout' && logType !== 'stderr'){
-        return ctx.body = {
-            'error': 'Log Type must be stdout or stderr'
-        }
-    }
-    const app =  await describeApp(appName)
-    const filePath = logType === 'stdout' ? app.pm_out_log_path: app.pm_err_log_path
-    let logs = await readLogsReverse({filePath, nextKey})
-    logs.lines = logs.lines.map(log => {
-        return  ansiConvert.toHtml(log)
-    }).join('<br/>')
-    return ctx.body = {
-        logs
-    };
+router.get("/logout", (ctx) => {
+  ctx.session = null;
+  return ctx.redirect("/login");
 });
 
-router.post('/api/apps/:appName/reload', isAuthenticated, async (ctx) => {
-    try{
-        let { appName } = ctx.params
-        let apps =  await reloadApp(appName)
-        if(Array.isArray(apps) && apps.length > 0){
-            return ctx.body = {
-                success: true
-            }
-        }
-        return ctx.body = {
-            success: false
-        }
-    }
-    catch(err){
-        return ctx.body = {
-            'error':  err
-        }
-    }
+router.get("/apps/:appName", isAuthenticated, async (ctx) => {
+  const { appName } = ctx.params;
+  let app = await describeApp(appName);
+  if (app) {
+    app.git_branch = await getCurrentGitBranch(app.pm2_env_cwd);
+    app.git_commit = await getCurrentGitCommit(app.pm2_env_cwd);
+    app.env_file = await getEnvFileContent(app.pm2_env_cwd);
+    const stdout = await readLogsReverse({ filePath: app.pm_out_log_path });
+    const stderr = await readLogsReverse({ filePath: app.pm_err_log_path });
+    stdout.lines = stdout.lines
+      .map((log) => {
+        return ansiConvert.toHtml(log);
+      })
+      .join("<br/>");
+    stderr.lines = stderr.lines
+      .map((log) => {
+        return ansiConvert.toHtml(log);
+      })
+      .join("<br/>");
+    return await ctx.render("apps/app", {
+      app,
+      logs: {
+        stdout,
+        stderr,
+      },
+    });
+  }
+  return ctx.redirect("/apps");
 });
 
-router.post('/api/apps/:appName/restart', isAuthenticated,  async (ctx) => {
-    try{
-        let { appName } = ctx.params
-        let apps =  await restartApp(appName)
-        if(Array.isArray(apps) && apps.length > 0){
-            return ctx.body = {
-                success: true
-            }
-        }
-        return ctx.body = {
-            success: false
-        }
-    }
-    catch(err){
-        console.log(err)
-        return ctx.body = {
-            'error':  err
-        }
-    }
+router.get("/api/apps/:appName/logs/:logType", isAuthenticated, async (ctx) => {
+  const { appName, logType } = ctx.params;
+  const { linePerRequest, nextKey } = ctx.query;
+  if (logType !== "stdout" && logType !== "stderr") {
+    return (ctx.body = {
+      error: "Log Type must be stdout or stderr",
+    });
+  }
+  const app = await describeApp(appName);
+  const filePath =
+    logType === "stdout" ? app.pm_out_log_path : app.pm_err_log_path;
+  let logs = await readLogsReverse({ filePath, nextKey });
+  logs.lines = logs.lines
+    .map((log) => {
+      return ansiConvert.toHtml(log);
+    })
+    .join("<br/>");
+  return (ctx.body = {
+    logs,
+  });
 });
 
-router.post('/api/apps/:appName/stop', isAuthenticated, async (ctx) => {
-    try{
-        let { appName } = ctx.params
-        let apps =  await stopApp(appName)
-        if(Array.isArray(apps) && apps.length > 0){
-            return ctx.body = {
-                success: true
-            }
-        }
-        return ctx.body = {
-            success: false
-        }
+router.post("/api/apps/:appName/reload", isAuthenticated, async (ctx) => {
+  try {
+    let { appName } = ctx.params;
+    let apps = await reloadApp(appName);
+    if (Array.isArray(apps) && apps.length > 0) {
+      return (ctx.body = {
+        success: true,
+      });
     }
-    catch(err){
-        return ctx.body = {
-            'error':  err
-        }
+    return (ctx.body = {
+      success: false,
+    });
+  } catch (err) {
+    return (ctx.body = {
+      error: err,
+    });
+  }
+});
+
+router.post("/api/apps/:appName/restart", isAuthenticated, async (ctx) => {
+  try {
+    let { appName } = ctx.params;
+    let apps = await restartApp(appName);
+    if (Array.isArray(apps) && apps.length > 0) {
+      return (ctx.body = {
+        success: true,
+      });
     }
+    return (ctx.body = {
+      success: false,
+    });
+  } catch (err) {
+    console.log(err);
+    return (ctx.body = {
+      error: err,
+    });
+  }
+});
+
+router.post("/api/apps/:appName/stop", isAuthenticated, async (ctx) => {
+  try {
+    let { appName } = ctx.params;
+    let apps = await stopApp(appName);
+    if (Array.isArray(apps) && apps.length > 0) {
+      return (ctx.body = {
+        success: true,
+      });
+    }
+    return (ctx.body = {
+      success: false,
+    });
+  } catch (err) {
+    return (ctx.body = {
+      error: err,
+    });
+  }
 });
 
 module.exports = router;
